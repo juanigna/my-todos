bd218d2ef2a3d5dfff2431f8852919e46a183df7
JuaniSO <juan.bocchi@sphereone.xyz>
2024-02-16 16:53:10 -0300

    changing photo

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
 
                 <div className="mt-8 text-center">
                     {/* Next/Image */}
-                    <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" alt="" className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" width={40} height={40} />
+                    <Image src="https://avatars.githubusercontent.com/u/21323876?v=4" className="m-auto w-32 rounded-full" alt="tailus logo" width={150} height={150} />
                     <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
                     <span className="hidden text-gray-400 lg:block">Admin</span>
                 </div>
